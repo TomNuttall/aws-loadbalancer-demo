6df548209b09ca47a6738f6a71a167898a1a051f
Tom <tsnuttall@gmail.com>
2024-06-22 20:14:02 +0100

    app: Test update

--- app/src/controllers/index.controller.ts
+++ app/src/controllers/index.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 class IndexController {
   async get(req: Request, res: Response, next: any) {
     try {
-      res.render('home', { title: 'Test App', message: 'Welcome!' })
+      res.render('home', { title: 'Test App', message: 'Welcome! v1' })
     } catch (err) {
       // console.error(`Error`, err.message)
       next(err)
