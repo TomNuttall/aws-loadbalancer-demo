47d92c80f38de9821ce6759ed411110f79996706
Tom <tsnuttall@gmail.com>
2024-06-22 20:21:41 +0100

    app: Update install script

--- app/scripts/install_dependencies.sh
+++ app/scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+source ~/.bashrc
+nvm install node
+npm install yarn -g
+source ~/.bashrc
+yarn global add pm2
 cd app
 yarn install
