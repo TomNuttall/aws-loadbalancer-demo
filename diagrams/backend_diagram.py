36a27583530c4827892b7edfc59dfb6e1c3b8c38
Tom <tsnuttall@gmail.com>
2024-06-23 12:49:48 +0100

    diagrams: Update diagram with route53

--- diagrams/backend_diagram.py
+++ diagrams/backend_diagram.py
@@ -12,24 +12,23 @@ with Diagram("", filename="backend_diagram", outformat="png"):
     github_action = GithubActions("Github Action")
 
     with Cluster("AWS"):
-        # route_53 = Route53("Route53")
-        # route_53 - ACM("ACM")
+        route_53 = Route53("Route53")
 
         s3_bucket = S3("S3")
         code_deploy_group = Codedeploy("Code Deploy")
 
         with Cluster(""):
             app_load_balancer = ALB("App Load Balancer")
+            app_load_balancer - ACM("ACM")
 
             with Cluster("Target Group"):
                 auto_scaling_group = AutoScaling("Auto Scaling Group")
-                app_load_balancer >> Edge(label="") >> auto_scaling_group
+                app_load_balancer >> Edge(label="HTTPS") >> auto_scaling_group
                 auto_scaling_group - [
                     EC2("EC2 Instance"), EC2("EC2 Instance")]
 
-        # route_53 >> app_load_balancer
+        route_53 >> app_load_balancer
 
-    # user >> Edge(label="") >> route_53
-    user >> app_load_balancer
+    user >> Edge(label="") >> route_53
     github_action >> Edge(label="Deploys backend app to S3") >> s3_bucket >> Edge(
         label="Deploys to target group") >> code_deploy_group >> auto_scaling_group
