5715fd02a2bf30d1eb1e8546fe7c46a518fb1c5c
Tom <tsnuttall@gmail.com>
2024-06-23 13:22:03 +0100

    app: Install script tweak

--- app/scripts/install_dependencies.sh
+++ app/scripts/install_dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-source ~/.bashrc
-nvm install --lts
 sudo chown -R $USER:$USER app
-npm install -g pm2
 cd app
 npm install
